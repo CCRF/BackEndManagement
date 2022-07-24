81cc8462c4744ea842aa3c5c3950b1989a5bb0ac
LXJ <526392973@qq.com>
2022-07-24 20:05:16 +0800

    test

--- src/main/java/cn/eud/guet/backendmanagement/controller/TestController02.java
+++ src/main/java/cn/eud/guet/backendmanagement/controller/TestController02.java
@@ -1,4 +1,11 @@
 package cn.eud.guet.backendmanagement.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class TestController02 {
+
+    @RequestMapping("/admin/login2")
+    public String login2(){
+        return "loginTest2";
+    }
 }
