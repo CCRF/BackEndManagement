8b9012899e0c0db3dc192922966e3af9b74bc256
LXJ <526392973@qq.com>
2022-07-24 12:54:08 +0800

    Initial commit

---
+++ src/main/java/cn/eud/guet/backendmanagement/BackEndManagementApplication.java
@@ -0,0 +1,13 @@
+package cn.eud.guet.backendmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/eud/guet/backendmanagement/BackEndManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.eud.guet.backendmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
