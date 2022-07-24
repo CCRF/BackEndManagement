ec404a8d8c5435ce03717ad0b7af3d667f822c22
LXJ <526392973@qq.com>
2022-07-24 19:36:24 +0800

    testMastertoBranch

--- src/main/java/cn/eud/guet/backendmanagement/bean/Test2.java
+++ src/main/java/cn/eud/guet/backendmanagement/bean/Test2.java
@@ -2,5 +2,5 @@ package cn.eud.guet.backendmanagement.bean;
 
 public class Test2 {
     private int hjkId;
-
+    private String lxjId;
 }
---
+++ src/main/java/cn/eud/guet/backendmanagement/bean/TestMaster.java
@@ -0,0 +1,5 @@
+package cn.eud.guet.backendmanagement.bean;
+
+public class TestMaster {
+    private int testMasterId;
+}
