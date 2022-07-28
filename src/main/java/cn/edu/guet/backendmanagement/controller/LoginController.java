6a5b2c9e4d6766e4300e4e57d2438a65fb221796
LXJ <526392973@qq.com>
2022-07-28 12:40:25 +0800

    init

---
+++ src/main/java/cn/edu/guet/backendmanagement/controller/LoginController.java
@@ -0,0 +1,50 @@
+package cn.edu.guet.backendmanagement.controller;
+
+import cn.edu.guet.backendmanagement.bean.LoginBean;
+import cn.edu.guet.backendmanagement.bean.SysUser;
+import cn.edu.guet.backendmanagement.http.HttpResult;
+import cn.edu.guet.backendmanagement.security.JwtAuthenticationToken;
+import cn.edu.guet.backendmanagement.service.SysUserService;
+import cn.edu.guet.backendmanagement.util.PasswordUtils;
+import cn.edu.guet.backendmanagement.util.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping(value = "/login")
+    public HttpResult login(@RequestBody LoginBean loginBean, HttpServletRequest request) throws IOException {
+        String username = loginBean.getUsername();
+        String password = loginBean.getPassword();
+        System.out.println("username = " + username + " password = " + password);
+        // 用户信息
+        SysUser user = sysUserService.findByName(username);
+        // 账号不存在、密码错误
+        if (user == null) {
+            return HttpResult.error("账号不存在");
+        }
+        if (!PasswordUtils.matches(user.getSalt(), password, user.getPassword())) {
+            return HttpResult.error("密码不正确");
+        }
+        // 账号锁定
+        if (user.getStatus() == 0) {
+            return HttpResult.error("账号已被锁定,请联系管理员");
+        }
+        // 系统登录认证
+        JwtAuthenticationToken token = SecurityUtils.login(request, username, password, authenticationManager);
+        return HttpResult.ok(token);
+    }
+}
