e225f733a11aff57886e3137a545b07f07293dce
ertong <2841119399@qq.com>
2022-08-06 22:24:28 +0800

    实现菜单管理的增删查改

--- src/main/java/cn/edu/guet/backendmanagement/mapper/SysMenuMapper.java
+++ src/main/java/cn/edu/guet/backendmanagement/mapper/SysMenuMapper.java
@@ -1,11 +1,10 @@
 package cn.edu.guet.backendmanagement.mapper;
 
+import java.util.Date;
 import java.util.List;
 
 import cn.edu.guet.backendmanagement.bean.SysMenu;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 /**
  * @Author Liwei
@@ -22,4 +21,18 @@ public interface SysMenuMapper {
 
     @Select("select count(*) from sys_menu WHERE del_flag!=-1")
     int selectTotalCount();
+
+    List<SysMenu> searchMsg(@Param("msg") String msg);
+
+    @Select("select * from sys_menu where del_flag!=-1")
+    List<SysMenu> count();
+
+    @Update("update sys_menu set del_flag=-1 where id=#{id}")
+    void deleteMsg(Long id);
+
+    @Insert("insert into sys_menu(name, parent_id, url, type, order_num,create_by, create_time, del_flag) values (#{name},#{parentId},#{url},#{type},#{orderNum},#{createBy},#{createTime},#{delFlag})")
+    int addMsg(SysMenu sysMenu);
+
+    @Update("update sys_menu set name=#{name}, url=#{url},last_update_time=#{lastUpdateTime},last_update_by=#{lastUpdateBy} where id=#{id}")
+    void updateName(String name, String url, Long id, Date lastUpdateTime, String lastUpdateBy);
 }
--- src/main/java/cn/edu/guet/backendmanagement/service/SysMenuService.java
+++ src/main/java/cn/edu/guet/backendmanagement/service/SysMenuService.java
@@ -2,6 +2,8 @@ package cn.edu.guet.backendmanagement.service;
 
 import cn.edu.guet.backendmanagement.bean.PageBean;
 import cn.edu.guet.backendmanagement.bean.SysMenu;
+import cn.edu.guet.backendmanagement.bean.SysRole;
+import cn.edu.guet.backendmanagement.http.HttpResult;
 
 import java.util.List;
 
@@ -31,4 +33,12 @@ public interface SysMenuService {
     List<SysMenu> findTree(String userName, int menuType);
 
     PageBean<SysMenu> findAll(int page, int size);
+
+    List<SysMenu> searchMsg(String msg);
+
+    HttpResult deleteMsg(Long id);
+
+    int addMsg(SysMenu sysMenu);
+
+    HttpResult updateName(SysMenu sysMenu);
 }
