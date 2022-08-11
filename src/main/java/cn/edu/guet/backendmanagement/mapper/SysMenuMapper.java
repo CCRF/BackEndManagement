a791f5e9a20b80cf1e0cf657c64f4a04c8f439d1
ertong <2841119399@qq.com>
2022-08-11 16:59:41 +0800

    对菜单管理部分接口的修改

--- src/main/java/cn/edu/guet/backendmanagement/mapper/SysMenuMapper.java
+++ src/main/java/cn/edu/guet/backendmanagement/mapper/SysMenuMapper.java
@@ -27,11 +27,11 @@ public interface SysMenuMapper {
     @Update("update sys_menu set del_flag=-1 where id=#{id}")
     void deleteMsg(Long id);
 
-    @Insert("insert into sys_menu(name, parent_id, url, type, perms, order_num, create_by, create_time, del_flag) values (#{name},#{parentId},#{url},#{type},#{perms},#{orderNum},#{createBy},#{createTime},#{delFlag})")
+    @Insert("insert into sys_menu(name, parent_id, type, order_num, create_by, create_time, del_flag) values (#{name},#{parentId},#{type},#{orderNum},#{createBy},#{createTime},#{delFlag})")
     int addMsg(SysMenu sysMenu);
 
-    @Update("update sys_menu set name=#{name}, url=#{url},last_update_time=#{lastUpdateTime},last_update_by=#{lastUpdateBy} where id=#{id}")
-    void updateName(String name, String url, Long id, Date lastUpdateTime, String lastUpdateBy);
+    @Update("update sys_menu set name=#{name},last_update_time=#{lastUpdateTime},last_update_by=#{lastUpdateBy} where id=#{id}")
+    void updateName(String name, Long id, Date lastUpdateTime, String lastUpdateBy);
 
 
 }
